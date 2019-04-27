ea6b360ca362863de7f84f312b5e56847f05b899
Junho Kim <takis0112@gmail.com>
2019-04-28 02:29:27 +0900

    add global_context_block

--- ops.py
+++ ops.py
@@ -454,6 +454,44 @@ def convolution_block_attention(x, channels, ratio=16, use_bias=True, sn=False, 
             return x
 
 
+def global_context_block(x, channels, use_bias=True, sn=False, scope='gc_block'):
+    with tf.variable_scope(scope):
+        with tf.variable_scope('context_modeling'):
+            bs, h, w, c = x.get_shape().as_list()
+            input_x = x
+            input_x = hw_flatten(input_x)  # [N, H*W, C]
+            input_x = tf.transpose(input_x, perm=[0, 2, 1])
+            input_x = tf.expand_dims(input_x, axis=1)
+
+            context_mask = conv(x, channels=1, kernel=1, stride=1, use_bias=use_bias, sn=sn, scope='conv')
+            context_mask = hw_flatten(context_mask)
+            context_mask = tf.nn.softmax(context_mask, axis=1)  # [N, H*W, 1]
+            context_mask = tf.transpose(context_mask, perm=[0, 2, 1])
+            context_mask = tf.expand_dims(context_mask, axis=-1)
+
+            context = tf.matmul(input_x, context_mask)
+            context = tf.reshape(context, shape=[bs, 1, 1, c])
+
+        with tf.variable_scope('transform_0'):
+            context_transform = conv(context, channels, kernel=1, stride=1, use_bias=use_bias, sn=sn, scope='conv_0')
+            context_transform = layer_norm(context_transform)
+            context_transform = relu(context_transform)
+            context_transform = conv(context_transform, channels=c, kernel=1, stride=1, use_bias=use_bias, sn=sn, scope='conv_1')
+            context_transform = sigmoid(context_transform)
+
+            x = x * context_transform
+
+        with tf.variable_scope('transform_1'):
+            context_transform = conv(context, channels, kernel=1, stride=1, use_bias=use_bias, sn=sn, scope='conv_0')
+            context_transform = layer_norm(context_transform)
+            context_transform = relu(context_transform)
+            context_transform = conv(context_transform, channels=c, kernel=1, stride=1, use_bias=use_bias, sn=sn, scope='conv_1')
+
+            x = x + context_transform
+
+        return x
+
+
 ##################################################################################
 # Normalization
 ##################################################################################
@@ -728,7 +766,6 @@ def histogram_loss(x, y):
 
     return hist_loss
 
-
 def get_histogram(img, bin_size=0.2):
     hist_entries = []
 
@@ -788,9 +825,9 @@ def dice_loss(n_classes, logits, labels):
     :param labels: [batch_size, m, n, 1] int32, class label
     :return:
     """
-    
+
     # https://github.com/keras-team/keras/issues/9395
-    
+
     smooth = 1e-7
     dtype = tf.float32
 
